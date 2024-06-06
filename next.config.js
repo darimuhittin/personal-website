5fcd02538f507e28224a75c474cf33cbb1ac0ab9
Muhittin Dari <darimuhittin@gmail.com>
2024-06-06 10:14:09 +0300

    a

--- next.config.js
+++ next.config.js
@@ -7,11 +7,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/.well-known/apple-app-site-association',
+        source: "/.well-known/apple-app-site-association",
         headers: [
           {
-            key: 'Content-Type',
-            value: 'application/json',
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
           },
         ],
       },
